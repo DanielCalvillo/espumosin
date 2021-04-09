93507a96457ffa9d29926e027a45bb371d3a7a4f
DanielCalvillo <danielcalvillomancilla123@gmail.com>
2021-04-09 04:30:32 -0500

    typescript fix

--- src/Components/ForgotPassword.tsx
+++ src/Components/ForgotPassword.tsx
@@ -1,15 +1,15 @@
-import * as React from 'react';
-import { Field, Form, FormSpy } from 'react-final-form';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from './modules/components/Typography';
-import AppFooter from './modules/views/AppFooter';
-import AppAppBar from './modules/views/AppAppBar';
-import AppForm from './modules/views/AppForm';
-import { email, required } from './modules/form/validation';
-import RFTextField from './modules/form/RFTextField';
-import FormButton from './modules/form/FormButton';
-import FormFeedback from './modules/form/FormFeedback';
-import withRoot from './modules/withRoot';
+import * as React from "react";
+import { Field, Form, FormSpy } from "react-final-form";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "./modules/components/Typography";
+import AppFooter from "./modules/views/AppFooter";
+import AppAppBar from "./modules/views/AppAppBar";
+import AppForm from "./modules/views/AppForm";
+import { email, required } from "./modules/form/validation";
+import RFTextField from "./modules/form/RFTextField";
+import FormButton from "./modules/form/FormButton";
+import FormFeedback from "./modules/form/FormFeedback";
+import withRoot from "./modules/withRoot";
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -29,7 +29,7 @@ function ForgotPassword() {
   const [sent, setSent] = React.useState(false);
 
   const validate = (values: { [index: string]: string }) => {
-    const errors = required(['email'], values);
+    const errors = required(["email"], values);
 
     if (!errors.email) {
       const emailError = email(values.email);
@@ -55,7 +55,7 @@ function ForgotPassword() {
           </Typography>
           <Typography variant="body2" align="center">
             {"Enter your email address below and we'll " +
-              'send you a link to reset your password.'}
+              "send you a link to reset your password."}
           </Typography>
         </React.Fragment>
         <Form
@@ -63,13 +63,13 @@ function ForgotPassword() {
           subscription={{ submitting: true }}
           validate={validate}
         >
-          {({ handleSubmit: handleSubmit2, submitting }) => (
+          {({ handleSubmit: handleSubmit2 }) => (
             <form onSubmit={handleSubmit2} className={classes.form} noValidate>
               <Field
                 autoFocus
                 autoComplete="email"
                 component={RFTextField}
-                disabled={submitting || sent}
+                // disabled={submitting || sent}
                 fullWidth
                 label="Email"
                 margin="normal"
@@ -77,7 +77,7 @@ function ForgotPassword() {
                 required
                 size="large"
               />
-              <FormSpy subscription={{ submitError: true }}>
+              {/* <FormSpy subscription={{ submitError: true }}>
                 {({ submitError }) =>
                   submitError ? (
                     <FormFeedback className={classes.feedback} error>
@@ -85,15 +85,15 @@ function ForgotPassword() {
                     </FormFeedback>
                   ) : null
                 }
-              </FormSpy>
+              </FormSpy> */}
               <FormButton
                 className={classes.button}
-                disabled={submitting || sent}
+                // disabled={submitting || sent}
                 size="large"
                 color="secondary"
                 fullWidth
               >
-                {submitting || sent ? 'In progress…' : 'Send reset link'}
+                Send reset link
               </FormButton>
             </form>
           )}
